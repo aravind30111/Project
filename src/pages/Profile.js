570b327e39d2bf005fff45a6b1c18b842b20de58
aravind30111 <aravind30111@gmail.com>
2022-05-17 15:12:44 +0530

    Profile Pic Changed

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Camera from "../components/svg/Camera";
-import Img from "../image1.jpg";
+import Img from "../Logo.jpeg";
 import { storage, db, auth } from "../firebase";
 import {
   ref,
